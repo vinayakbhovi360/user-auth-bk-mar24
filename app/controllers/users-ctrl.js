4ff8493f7b90678d6838db7b0a4831669d4a9b7a
vinayakbhovi360 <vinayakbhovi360@gmail.com>
2024-08-02 13:52:39 +0530

    hash user password

--- app/controllers/users-ctrl.js
+++ app/controllers/users-ctrl.js
@@ -1,6 +1,11 @@
+// git app .
+// git commit -m "users controller -register action"
+// git push origin main
+
 
 import User from "../models/user-model.js";
 import { validationResult } from "express-validator";
+import bcryptjs from "bcryptjs"
 const usersCtrl  = {}
 
 usersCtrl.register = async (req,res) => {
@@ -11,6 +16,13 @@ usersCtrl.register = async (req,res) => {
     const body = req.body
     try{
         const user = new User(body)
+        console.log(user)
+        //salt
+        const salt = await bcryptjs.genSalt()// rounds 10 by diffult it will shuffle for 10 times.
+        //hash
+        const hash = await bcryptjs.hash(user.password,salt)
+        //update the user
+        user.password = hash
         await user.save()
         res.status(201).json(user)
 
